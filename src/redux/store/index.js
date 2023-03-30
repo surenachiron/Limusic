0f85f4c1538f380f0baf076c324a889b0800829d
[surena] <[mohammadkarimi.swctw@gmail.com]>
2023-03-30 18:27:13 +0430

    delete redux tools

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -4,8 +4,5 @@ import { reducers } from "../reducers/";
 
 export const store = createStore(
     reducers,
-    compose(applyMiddleware(thunk),
-        window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
+    compose(applyMiddleware(thunk))
 );
