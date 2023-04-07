196bfebd9b8b76e308eaab31edf76098a7c6d325
[surena] <[mohammadkarimi.swctw@gmail.com]>
2023-04-07 16:55:58 +0330

    Added music player functionality including play/pause, next/previous track navigation, repeat and shuffle options, and progress bar with remaining and total time display.

--- src/pages/Artist.jsx
+++ src/pages/Artist.jsx
@@ -7,7 +7,7 @@ import Latestreleaseartist from '../component/artistpage/Latestreleaseartist'
 import Mapalbumbyartist from "../component/artistpage/Mapalbumbyartist";
 import Maptopsongbyartist from "../component/artistpage/Maptopsongbyartist";
 import { useEffect } from "react";
-import { getalbumsartist, getalldetailsartist, getlatestreleaseartist, gettopsongartist } from "../services/useEndpointspageartist";
+import { getalbumsartist, getalldetailsartist, getlatestreleaseartist, gettopsongartist } from "../services/useEndpointspageartist.js";
 import { initializeartist, initializetopsongartist, initializelatestreleaseartist, initializealbumsartist } from "../redux/actions/artist";
 import { useLocation } from "react-router-dom"; 
 
