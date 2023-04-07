f35cf29b099329a755356eb3517e154c36c8d18e
[surena] <[mohammadkarimi.swctw@gmail.com]>
2023-04-07 16:49:51 +0330

    Added music player functionality including play/pause, next/previous track navigation, repeat and shuffle options, and progress bar with remaining and total time display.

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -5,8 +5,6 @@ import { reducers } from "../reducers/";
 export const store = createStore(
     reducers,
     compose(
-        applyMiddleware(thunk),
-        window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__()
+        applyMiddleware(thunk)
     )
 );
